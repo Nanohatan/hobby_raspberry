f59c0c348d3899444775bb1bf7e1905be52ad074
homma nonoka <e185730@ie.u-ryukyu.ac.jp>
2023-05-04 20:50:57 +0900

    add template rendaring

--- composetest/app.py
+++ composetest/app.py
@@ -3,6 +3,7 @@ import time
 import redis
 from flask import Flask
 
+import flask
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
@@ -19,5 +20,6 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    dic = {"title":"test"}
+    return flask.render_template("index.html",data=dic)
+
